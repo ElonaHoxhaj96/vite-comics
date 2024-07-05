70bb8d008b3e081d64636a63819e1a6223240a6a
ElonaHoxhaj96 <elona6915@gmail.com>
2024-07-05 19:31:30 +0200

    feat: Nav in header

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,14 @@
 import AppHeader from './components/AppHeader.vue'
 export default{
   name:'App',
-  components: AppHeader,
+  components:{
+    AppHeader,
+  } 
 }
 </script>
 
 <template>
-   <AppHeader/>
+  <AppHeader />
 </template>
 
 <style lang="scss"> 
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -6,5 +6,25 @@ export default {
 
 
 <template> 
-    <header></header>
+    <header>
+        <nav>
+            <div>
+                <img src="../assets/dc-logo.png" alt="logo">
+            </div>
+            <a href="#">CHARACTER</a>
+            <a href="#">MOVIES</a>
+            <a href="#">TV</a>
+            <a href="#">GAMES</a>
+            <a href="#">COLLECTIBLES</a>
+            <a href="#">VIDEOS</a>
+            <a href="#">FANS</a>
+            <a href="#">NEWS</a>
+            <a href="#">SHOP</a>
+        </nav>
+    </header>
 </template>
+
+<style lang="scss" scoped>
+
+
+</style>
