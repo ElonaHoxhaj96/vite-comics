0470073a0a1c10d464119b109e85ffe9d4b79d37
ElonaHoxhaj96 <elona6915@gmail.com>
2024-07-05 18:45:48 +0200

    feat: created vue files

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,16 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+import AppHeader from './components/AppHeader.vue'
+export default{
+  name:'App',
+  components: AppHeader,
+}
 </script>
 
 <template>
-  
-  <HelloWorld msg="Vite + Vue" />
+   <AppHeader/>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+<style lang="scss"> 
+@use './styles/general.scss' as *;
+
 </style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,10 @@
+<script>
+export default {
+    name: 'AppHeader',
+}
+</script>
+
+
+<template> 
+    <header></header>
+</template>
