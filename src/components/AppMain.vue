6e2ecad5a8231d5e404cf9d3c8dcc2aeb5bfedde
ElonaHoxhaj96 <elona6915@gmail.com>
2024-07-06 12:31:31 +0200

    Adding main section

---
+++ src/components/AppMain.vue
@@ -0,0 +1,28 @@
+<script>
+export default{
+    name: 'AppMain',
+}
+</script>
+
+
+<template> 
+    <main>
+        <div class="container">
+            <h1> --> CONTENT GOES HERE <--</h1>
+        </div>
+
+    </main>
+</template>
+
+<style>
+    main{
+        .container{
+            min-height: 150px;
+            background-color: black;
+                h1{
+                    color: white;
+                    padding-top: 5% ;
+                }
+        }
+    }
+</style>
